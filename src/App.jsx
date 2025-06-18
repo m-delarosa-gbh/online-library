7d7890cc81b3b57a41e56f8128a745b4e6849e8a
marivict <mvdelarosaramirez@gmail.com>
2025-06-18 15:36:04 -0400

    feat: create Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Container, Row, Col } from "react-bootstrap"
+import { HeaderNavigation } from "./layouts/headerNavigation"
 
 
 function App() {
   return (
     <>
-      
+        <HeaderNavigation />
     </>
   )
 }
---
+++ src/layouts/index.jsx
@@ -0,0 +1,1 @@
+import { HeaderNavigation } from "./headerNavigation";
