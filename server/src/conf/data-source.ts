e76532ebd97225f7368e704e979348e08737ce6a
marivict <mvdelarosaramirez@gmail.com>
2025-07-29 11:57:10 -0400

    feat:  configuring typeorm

---
+++ server/src/conf/data-source.ts
@@ -0,0 +1,16 @@
+import {DataSource} from "typeorm";
+import 'dotenv/config';
+import { Authors, Books, Categories, Formats } from "../entity/index.ts";
+
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 5432,
+    database: process.env.DB_NAME,
+    entities:[Authors, Categories, Books, Formats],
+    synchronize: true,
+    logging: false,
+})
